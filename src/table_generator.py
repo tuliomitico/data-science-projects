ef6d7af23f1d5b78dccc0a42a0445e071c8a5e52
tuliomitico <tuliofc2008@hotmail.com>
2022-07-14 12:00:01 -0300

    feat: add matplotlib tables fn

---
+++ src/table_generator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 13 12:06:36 2022
+
+@author: TÚLIO
+"""
+import typing as t
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def table_generator(data: t.Union[pd.DataFrame,np.ndarray]):
+    fig_background_color = "skyblue"
+    return
+    
