91211e4830283d1d19cb2112a3288d5285ff8ef0
tuliomitico <tuliofc2008@hotmail.com>
2022-09-14 13:55:01 -0300

    chore: add description, fix table generator fn

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # Data Science projects
 
+This is a repository that's contains some functionalities to help me remember when i need to generate some exploratory data analysis and make some machine learning pipelines. I hope maybe useful for someone just like me 😁.
 ## Technologies in use
 
-- `poetry`
 - `jupyter notebook`
+- `matplotlib`
+- `numpy`
+- `pandas`
+- `poetry`
+- `scikit-learn`
+- `seaborn`
+
+How to contribute
+---
+
+In progress..
--- src/table_generator.py
+++ src/table_generator.py
@@ -6,11 +6,43 @@ Created on Wed Jul 13 12:06:36 2022
 """
 import typing as t
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
 
-def table_generator(data: t.Union[pd.DataFrame,np.ndarray]):
+def table_generator(
+    data: t.Union[pd.DataFrame,np.ndarray], name: str, title: str, footnote: str
+) -> None:
+    """Generate a table and save into the disk.
+
+    Parameters
+    ----------
+    data : array-like, shape (``n_samples``,``n_features``) 
+        The data to generate the table.
+
+    Returns
+    -------
+    Image: png, jpeg, svg, etc.
+        The resulting image given the parameters and data.
+    """
     fig_background_color = "skyblue"
+    fig_border = "steelblue"
+
+    row_headers = data.index
+    cols_headers = data.columns
+
+    cell: np.ndarray = MinMaxScaler().fit_transform(data.values)
+
+    rcolors = plt.cm.BuPu(np.full(len(row_headers),.1))
+    ccolors = plt.cm.BuPu(np.full(len(cols_headers),.1))
+    cellcolors = plt.cm.summer(cell)
+
+    plt.figure(
+        linewidth = 2, 
+        edgecolor = fig_border, 
+        facecolor = fig_background_color,
+        tight_layout = {'pad': 1}
+    )
     return
     
